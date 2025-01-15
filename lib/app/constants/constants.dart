5965f2180480b178d470012027d3afbb11417dd6
yousefelsayed <eyousef464@gmail.com>
2025-01-15 16:20:10 +0200

    x

---
+++ lib/app/constants/constants.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// TextTheme textTheme = TextTheme(
+//   headlineLarge: GoogleFonts.sourceSerifPro(
+//     fontSize: 32,
+//     color: Colors.black87,
+//     fontWeight: FontWeight.bold,
+//   ),
+//   headlineMedium: GoogleFonts.sourceSerifPro(
+//     fontSize: 20,
+//     color: Colors.black87,
+//     fontWeight: FontWeight.w800,
+//   ),
+//   headlineSmall: GoogleFonts.sourceSerifPro(
+//     fontSize: 18,
+//     fontWeight: FontWeight.w400,
+//     color: Colors.black87,
+//   ),
+//   titleLarge: GoogleFonts.poppins(
+//     fontSize: 16,
+//     color: Colors.black87,
+//     fontWeight: FontWeight.w400,
+//   ),
+//   titleMedium: GoogleFonts.poppins(
+//     color: Colors.grey,
+//     fontWeight: FontWeight.w400,
+//   ),
+// );
+
+class AppColors {
+  static Color lightBlue = const Color(0xffCFEDEF);
+  static Color black = Colors.black87;
+}
+
+List<Color> boxColors = const [
+  Color(0xffCEEDEF),
+  Color(0xffBFBFBF),
+  Color(0xffEBECF1),
+  Color(0xffFDF7DD),
+  Color(0xffF9CFE3),
+  Color(0xffFBEBC7),
+  Color(0xffE9E8E6)
+];
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:ebook/app/notifiers/app_notifier.dart';
+import 'package:ebook/presentation/screens/main_screen.dart';
+import 'package:ebook/presentation/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'app/constants/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppNotifier()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // theme: ThemeData(textTheme: textTheme),
+        home: const MainScreen(),
+      ),
+    );
+  }
+}
